5d60e0e9eb5195a9d0353f02865fe0b39054213a
motoyasu.saburi <motoyasu.saburi@bizreach.co.jp>
2018-01-14 23:06:43 +0900

    proxy_handlerのプロトタイプ

--- tcp/proxy.py
+++ tcp/proxy.py
@@ -4,6 +4,9 @@ import sys
 import  socket
 import threading
 
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+  # TODO
+
 def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
